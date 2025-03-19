ace72d3a4729239307a92c91f1383c9cc4e94ed0
bautomass <33027482+bautomass@users.noreply.github.com>
2025-03-19 09:36:16 +0200

    modified:   src/components/home/Services.tsx

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -327,7 +327,7 @@ export default function Navbar() {
                   },
                 }}
               >
-                {navLinks.map((link, _index) => (
+                {navLinks.map((link) => (
                   <motion.div
                     key={link.href}
                     variants={{
